a9b09dd31c22c096ed15270d0a2dbcd7f71584c2
Louisa Price <louisaprice0@gmail.com>
2022-08-14 19:57:27 +0100

    Filled in missing latitude and longtitude values

--- sitelocationdata.py
+++ sitelocationdata.py
@@ -1,34 +1,22 @@
+from math import nan
+from pickle import TRUE
+from typing import no_type_check_decorator
 from OSGridConverter import grid2latlong
 import pandas as pd
-site_loc = pd.read_csv('ukbms2020csv/ukbmssitelocationdata2020.csv', encoding='cp1252')
+site_loc = pd.read_csv('updated_sitelocation.csv', encoding='cp1252')
 
-# def get_lat(df):
-#     l = grid2latlong(df['Gridreference'])
-#     return l.latitude
+#after using gridreference finder.com there were missing values, compiled into list below
+ist = []
+for i in site_loc.index:
+    if pd.isna(site_loc['latitude'][i]):
+        ist.append ([site_loc['Site Number'][i],site_loc['Gridreference'][i],site_loc['Easting'][i],site_loc['Northing'][i]])
 
-# def get_long(df): 
-#     l = grid2latlong(df['Gridreference'])
-#     return l.longitude
+missing_gridref = pd.DataFrame(ist, columns = ['Site Number','Gridreference','Easting','Northing'])        
 
-# site_loc['latitude'] = site_loc.apply(get_lat(site_loc), axis=1)
-# print(site_loc.head())
+#print(missing_gridref.to_csv())
+site_loc.drop(['updatedgridref'], axis=1,inplace=True)
 
-grid_refs = site_loc['Gridreference'].tolist()
-print(str(grid_refs)
-
-# latitude = []
-# for i in grid_refs:
-#     l = grid2latlong(i)
-#     latitude.append(l.latitude)
-
-# longitude = []
-# for i in grid_refs:
-#     l = grid2latlong(i)
-#     latitude.append(l.longitude)
-
-# site_loc['latitude'] = latitude
-# site_loc['longtitude'] = longitude
-# print(site_loc.head())
-
-
-
+#loading missing gridreferences dropping unessecary columns
+missing_site_loc = pd.read_csv('missing_sitelocationdata.csv',encoding='cp1252')
+missing_site_loc.drop(['ind','updatedgridref'], axis =1, inplace=True)
+print(missing_site_loc.head())
