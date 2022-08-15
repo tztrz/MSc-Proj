f18dec802b581788efbadf197b295309b6b66b9f
Louisa Price <louisaprice0@gmail.com>
2022-08-15 18:13:58 +0100

    Final site location data with correct latitude and longtitude produced

--- sitelocationdata.py
+++ sitelocationdata.py
@@ -3,6 +3,7 @@ from pickle import TRUE
 from typing import no_type_check_decorator
 from OSGridConverter import grid2latlong
 import pandas as pd
+import os
 site_loc = pd.read_csv('updated_sitelocation.csv', encoding='cp1252')
 
 #after using gridreference finder.com there were missing values, compiled into list below
@@ -19,4 +20,11 @@ site_loc.drop(['updatedgridref'], axis=1,inplace=True)
 #loading missing gridreferences dropping unessecary columns
 missing_site_loc = pd.read_csv('missing_sitelocationdata.csv',encoding='cp1252')
 missing_site_loc.drop(['ind','updatedgridref'], axis =1, inplace=True)
-print(missing_site_loc.head())
+
+site_loc = site_loc.set_index('Site Number')
+site_loc.update(missing_site_loc.set_index('Site Number'), join = 'left', overwrite=True)
+site_loc.reset_index(inplace=True)
+#Final_siteloc = pd.merge(site_loc,missing_site_loc,on='Site Number', how='outer')
+
+cwd = os.getcwd
+site_loc.to_csv('final_ukbmssitelocationdata2020.csv',index=False)
