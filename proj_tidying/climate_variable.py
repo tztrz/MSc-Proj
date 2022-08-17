bff820a35cf66f3ed65c8678e2f4f77ecfb43a3e
Louisa Price <louisaprice0@gmail.com>
2022-08-17 15:57:40 +0100

    Moved file locations, loaded climate data into exploration notebook

---
+++ proj_tidying/climate_variable.py
@@ -0,0 +1,23 @@
+#Importing all raw nc climate variable files and storing into csv 
+import xarray as xr
+import glob
+import pandas as pd
+
+dir_mean_temp = '/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/annual mean air temp'
+
+# mean_annual_airtemp = pd.DataFrame()
+# for filename in os.listdir(dir_mean_temp):
+#     f = os.path.join(dir_mean_temp,filename)
+#     if os.path.isfile(f):
+#         #print(f)
+#         file_open = xr.open_dataset(f)
+#         file_open_todf = file_open.to_dataframe()
+#         mean_annual_airtemp =mean_annual_airtemp.append(file_open_todf)
+
+# mean_annual_airtemp.to_csv('meanannualairtemp.csv',encoding='utf-8')
+
+ #loading all climate variable data from 1973 - 2020 into one dataframe, saving to csv for exploration
+filenames = glob.glob('/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/*/*.nc')
+clim_data = xr.open_mfdataset(filenames)
+clim_data_df = clim_data.to_dataframe()
+#clim_data_df.to_csv('/Users/Louisa/Desktop/climdata.csv') 
