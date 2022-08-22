dba7a49f232e341214ec6a60c55a3f5dc43e2342
Louisa Price <louisaprice0@gmail.com>
2022-08-22 21:32:10 +0100

    python file to notebook

--- proj_tidying/climate_variable.py
+++ proj_tidying/climate_variable.py
@@ -1,8 +1,9 @@
-#Importing all raw nc climate variable files and storing into csv 
+#%%
+#Imports
 import xarray as xr
 import glob
 import pandas as pd
-
+#%%
 dir_mean_temp = '/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/annual mean air temp'
 
 # mean_annual_airtemp = pd.DataFrame()
@@ -16,8 +17,18 @@ dir_mean_temp = '/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/annual
 
 # mean_annual_airtemp.to_csv('meanannualairtemp.csv',encoding='utf-8')
 
+#%%
  #loading all climate variable data from 1973 - 2020 into one dataframe, saving to csv for exploration
 filenames = glob.glob('/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/*/*.nc')
 clim_data = xr.open_mfdataset(filenames)
-clim_data_df = clim_data.to_dataframe()
+#clim_data_df = clim_data.to_dataframe()
 #clim_data_df.to_csv('/Users/Louisa/Desktop/climdata.csv') 
+
+#%%
+print(clim_data.variables.keys())
+#39.19
+
+#%%
+lat = clim_data.variables['latitude'][:]
+lon = clim_data.variables['longitude'][:]
+print(lat,lon)
