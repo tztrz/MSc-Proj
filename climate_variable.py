4b327488efa4da73a56fb2b3f707bc9089a2be4e
Louisa Price <louisaprice0@gmail.com>
2022-08-25 19:14:31 +0100

    Changes

--- climate_variable.py
+++ climate_variable.py
@@ -1,29 +1,31 @@
 #%%
-#Import
+#Imports
 import xarray as xr
 import glob
 import pandas as pd
 #%%
-dir_mean_temp = '/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/annual mean air temp'
-#%%
- #loading all climate variable data from 1973 - 2020 into one dataframe, saving to csv for exploration
+'''Loading all climate variable data from 1973 - 2020 into one dataframe, saving to csv for exploration'''
 #filenames = glob.glob('/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/annual mean air temp/*.nc')
 #clim_data_airtemp = xr.open_mfdataset(filenames)
 #clim_data_df = clim_data.to_dataframe()
 #clim_data_df.to_csv('/Users/Louisa/Desktop/climdata.csv') 
 
-#%%
-#print(clim_data_airtemp.variables.keys())
-#39.19
-
-#%%
-# lat = clim_data_airtemp.variables['latitude'][:]
-# lon = clim_data_airtemp.variables['longitude'][:]
-# date = clim_data_airtemp.variables['']
-# print(lat,lon)
 # %%
+#Loading site_indices and completed location data (see  /Missing Site Location Data/sitelocationdata.py)
 site_indices = pd.read_csv('ukbms2020csv/ukbmssiteindices2020.csv',encoding='cp1252')
 site_location_data = pd.read_csv('final_ukbmssitelocationdata2020.csv', encoding='cp1252')
 #%%
+#Combining all site indices records with applicable coordinates, dropping records without associated location data
 site_indices_withcoords = pd.merge(site_indices,site_location_data[['Site Number','latitude','longitude']],on='Site Number',how='left')
+site_indices_withcoords.dropna(inplace=True)
 # %%
+#Loading climatic variable data for years 1973 - 2020
+airtemp_filenames = glob.glob('/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/annual mean air temp/*.nc')
+rainfall_filenames = glob.glob('/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/annual rainfall/*.nc')
+humidity_filenames = glob.glob('/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/relative humidity/*.nc')
+sunshine_filenames = glob.glob('/Users/Louisa/Desktop/MScProject/HADUKAnnual (1973-2020)/sunshine hours/*.nc')
+airtemp_dat = xr.open_mfdataset(airtemp_filenames)
+rainfall_dat = xr.open_mfdataset(rainfall_filenames)
+humidity_dat = xr.open_mfdataset(humidity_filenames)
+sunshine_dat = xr.open_mfdataset(sunshine_filenames)
+#%%
